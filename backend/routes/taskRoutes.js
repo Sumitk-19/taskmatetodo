c80fdb37427eb4c46035848c32640a2f8f5780d3
Sumitk-19 <sumitkumar40710@gmail.com>
2025-04-11 19:58:05 +0530

    fix: register POST /api/tasks route

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const taskController = require("../controllers/taskController");
 
 router.get("/", taskController.getTasks);
-router.post("/", taskController.createTask);
+router.post("/", taskController.createTask); 
 router.put("/:id", taskController.updateTask);
 router.delete("/:id", taskController.deleteTask);
 
